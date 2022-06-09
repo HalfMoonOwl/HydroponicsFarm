72795e6c4a7f28eed07e610c043885a5012d7685
HalfMoonOwl <99697453+HalfMoonOwl@users.noreply.github.com>
2022-06-09 17:37:40 -0600

    Add files via upload

---
+++ Component_Code/22-06-09/Solenoid.cpp
@@ -0,0 +1,33 @@
+#include "Arduino.h"
+#include "Solenoid.h"
+
+//float EC_LOW = 2.0;
+//float EC_HIGH = 4.0;
+extern float EC_LOW;
+extern float EC_HIGH;
+
+int SolenoidFSM(float currentEC, int Pin){
+  static int ECState = 0;
+  Serial.print("Solenoid State: ");
+    Serial.println(ECState);
+    Serial.println(currentEC);
+
+//  currentEC = currentEC;    // Need to initialize it and read in a value still. 
+  switch (ECState) {
+    case 0:   // Do NOT run motor. Waiting state
+    digitalWrite(Pin, LOW);
+      if(currentEC >= EC_HIGH || currentEC < 0.5){   // Too much fertalizer, turn water on.
+        ECState = 1;
+      }
+    break;
+
+    case 1:   // Run motor
+    digitalWrite(Pin, HIGH);
+      if (currentEC > 0 && currentEC < EC_HIGH){ //Plant has water turn off
+        ECState = 0;
+      }      
+    break;
+  }
+  return(ECState);
+
+}
