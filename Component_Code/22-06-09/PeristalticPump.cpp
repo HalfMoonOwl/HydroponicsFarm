7f5d01063fcb56c309b3bba300e03608e9743ffb
HalfMoonOwl <99697453+HalfMoonOwl@users.noreply.github.com>
2022-06-09 18:31:29 -0600

    Update PeristalticPump.cpp
    The state was changing to transition statments.

--- Component_Code/22-06-09/PeristalticPump.cpp
+++ Component_Code/22-06-09/PeristalticPump.cpp
@@ -10,6 +10,8 @@ void PeristalticPumpFSM(float currentEC, int Pin) {
   static int peristalticPumpState = 0;
     Serial.print("Peristaltic Pump State: ");
     Serial.println(peristalticPumpState);
+    Serial.print("EC: ");
+    Serial.println(currentEC);
     
 
   switch (peristalticPumpState) {
@@ -18,7 +20,7 @@ void PeristalticPumpFSM(float currentEC, int Pin) {
       //If we calculate the flow rate of the water pump we can have this run at a specific amount
       //  so that we can equalize the EC immediately
 
-      if (currentEC < EC_LOW) {
+      if (0.5 < currentEC < EC_LOW) {
         peristalticPumpState = 1; //below the threshold
       }
       else if (currentEC > EC_HIGH) {
@@ -28,13 +30,14 @@ void PeristalticPumpFSM(float currentEC, int Pin) {
         peristalticPumpState = 3; // in the threshold
       }
       break;
+      
     case 1: //below the threshold
       digitalWrite(Pin, HIGH);
-      delay(3200);
+      delay(500);
       digitalWrite(Pin, LOW);
       delay(1500);
       
-      if (currentEC == 0) {
+      if (currentEC < 0.5) {
         peristalticPumpState = 0;
       }
       else if (EC_LOW < currentEC <= EC_HIGH) {
@@ -47,22 +50,22 @@ void PeristalticPumpFSM(float currentEC, int Pin) {
     case 2: //above the threshold
       digitalWrite(Pin, LOW);
 
-      if (currentEC == 0) {
+      if (currentEC < 0.5) {
         peristalticPumpState = 0;
       }
       else if (EC_LOW < currentEC <= EC_HIGH) {
         peristalticPumpState = 3; 
       }
-      else if (currentEC < EC_LOW) {
+      else if (0.5 < currentEC < EC_LOW) {
         peristalticPumpState = 1;
       }
       break;
     case 3: // in the threshold
       //do nothing just keep checking if statements
-      if (currentEC == 0) {
+      if (currentEC < 0.5) {
         peristalticPumpState = 0;
       }
-      else if (currentEC < EC_LOW) {
+      else if (0.5 < currentEC < EC_LOW) {
         peristalticPumpState = 1;
       }
       else if (currentEC > EC_HIGH) {
